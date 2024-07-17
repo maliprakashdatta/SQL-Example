291e9a9d5842b4407c69788292f28da004f8987e
Mali Prakash Datta <152262211+maliprakashdatta@users.noreply.github.com>
2024-07-17 16:06:58 +0530

    Update employee.sql

--- employee.sql
+++ employee.sql
@@ -1,1 +1,15 @@
+CREATE TABLE employee
 
+USE empolyee
+
+CREATE TABLE empolyee` (
+  empolyee_id INT NOT NULL,
+  emp_name VARCHAR(45) NULL,
+  emp_address VARCHAR(45) NULL,
+  emp_dept_id VARCHAR(45) NOT NULL,
+  emp_manager_id VARCHAR(45) NOT NULL,
+  empolyeecol VARCHAR(45) NULL,
+  PRIMARY KEY (empolyee_id, emp_manager_id, emp_dept_id));
+
+SELECT * FROM empolyee;
+-----------------------------------------------------------------------------

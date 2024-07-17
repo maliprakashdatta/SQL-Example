632a7a0bc7f95301ac4b2dac8d89f2999943f2d3
Mali Prakash Datta <152262211+maliprakashdatta@users.noreply.github.com>
2024-07-17 18:56:47 +0530

    Update employee.sql

--- employee.sql
+++ employee.sql
@@ -30,8 +30,6 @@ SELECT * FROM department;
 ---------------------------------------------------------------------------
 
 INSERT INTO department (department_id,  department_name) VALUES ('1', '	Marketing');
-
-
 INSERT INTO department(department_id,  department_name) VALUES ('2','Human Resources');
 INSERT INTO department(department_id,  department_name) VALUES ('3','Marketing');
 INSERT INTO department(department_id,  department_name) VALUES ('4','Business Development');
@@ -43,6 +41,17 @@ INSERT INTO department(department_id,  department_name) VALUES ('9','Sales');
 INSERT INTO department(department_id,  department_name) VALUES ('10','Training');
 
 ---------------------------------------------------------------------------------------------------
+another Way--
+INSERT INTO department (department_id,  department_name) VALUES ('1', '	Marketing',
+'2','Human Resources',
+'3','Marketing',
+'4','Business Development',
+'5','Legal',
+'6','Accounting',
+'7','Support',
+'8','Product Management',
+'9','Sales',
+'10','Training');
 
 
 

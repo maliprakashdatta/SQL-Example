00e288817df0e5cbf01cb1c9f125dced362c9a25
Mali Prakash Datta <152262211+maliprakashdatta@users.noreply.github.com>
2024-07-17 18:27:05 +0530

    Update employee.sql

--- employee.sql
+++ employee.sql
@@ -20,3 +20,12 @@ INSERT INTO empolyee(empolyee_id, emp_name, emp_address, emp_dept_id, emp_manage
 
 select * from employee;
 ------------------------------------------------------------------------------
+//new table creation department.
+CREATE TABLE department (
+  department_id INT NOT NULL,
+ department_name VARCHAR(45) NULL,
+  PRIMARY KEY (department_id));
+
+SELECT * FROM department;
+---------------------------------------------------------------------------
+
